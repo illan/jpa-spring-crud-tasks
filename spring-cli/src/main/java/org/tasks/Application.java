a0e7ad7c852ae2136db318e53b6177abf3aadf91
illan palao <illanpalao@gmail.com>
2014-12-05 11:55:56 +0000

    release-v1.1

--- spring-cli/src/main/java/org/tasks/Application.java
+++ spring-cli/src/main/java/org/tasks/Application.java
@@ -4,16 +4,30 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
 
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
+//@Import(RepositoryRestMvcConfiguration.class)
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+         
+         
+        System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+        String[] beanNames = ctx.getBeanDefinitionNames();
+        java.util.Arrays.sort(beanNames);
+        for (String beanName : beanNames) {
+            System.out.println(beanName);
+        }
     }
-/*    
+  /*  
    @Bean
    public JacksonRepositoryPopulatorFactoryBean repositoryPopulator() {
 
@@ -24,6 +38,7 @@ public class Application {
     //factory.setObjectMapper(…);
     factory.setResources(new Resource[] { sourceData });
     return factory;
-  }
- */ 
+    
+   }
+  */
 }

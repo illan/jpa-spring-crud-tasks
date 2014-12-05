c0fea1f1dd6535456b3cc0100318a8eaa210175c
illan palao <illanpalao@gmail.com>
2014-12-05 14:33:54 +0000

    refactoring model

--- spring-cli/src/main/java/org/tasks/Application.java
+++ spring-cli/src/main/java/org/tasks/Application.java
@@ -8,6 +8,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguration;
+import org.tasks.model.domain.*;
+import org.tasks.model.repositories.*;
+import java.util.List;
 
 @Configuration
 @ComponentScan
@@ -16,16 +19,74 @@ import org.springframework.data.rest.webmvc.config.RepositoryRestMvcConfiguratio
 public class Application {
 
     public static void main(String[] args) {
-         ApplicationContext ctx = SpringApplication.run(Application.class, args);
+         ApplicationContext context = SpringApplication.run(Application.class, args);
          
          
         System.out.println("Let's inspect the beans provided by Spring Boot:");
 
-        String[] beanNames = ctx.getBeanDefinitionNames();
+        String[] beanNames = context.getBeanDefinitionNames();
         java.util.Arrays.sort(beanNames);
         for (String beanName : beanNames) {
             System.out.println(beanName);
         }
+        
+     System.out.println("-------------------------------");
+     System.out.println("SETUP CONTEXT");
+     System.out.println("-------------------------------");
+
+
+        OperarioRepository repository = context.getBean(OperarioRepository.class);
+        TareaRepository tareaRepository = context.getBean(TareaRepository.class);
+
+        // save a couple of Operarios
+
+  
+        Operario o = new Operario("Jack");
+        repository.save(o);
+        Direccion d = new Direccion();
+        d.setDescripcion("Direccion Tarea 1");
+        Tarea t=new Tarea("Tarea asignada a Jack");
+        t.setDireccion(d);
+        
+        t.setAsignado(o);
+
+        tareaRepository.save(t);
+        Ejecucion e=new Ejecucion(t,o,Tarea.Estado.COMPLETADA);
+        
+        context.getBean(EjecucionRepository.class).save(e);
+        tareaRepository.save(t);
+        Repository.save(o);
+        
+        repository.save(new Operario("Chloe"));
+        repository.save(new Operario("Bauer"));
+
+      
+        // fetch all customers
+        Iterable<Operario> customers = repository.findAll();
+        System.out.println("Operarios found with findAll():");
+        System.out.println("-------------------------------");
+        for (Operario customer : customers) {
+            System.out.println(customer);
+        }
+        System.out.println();
+
+        // fetch an individual customer by ID
+        Operario operario = repository.findOne(1L);
+        System.out.println("Operario found with findOne(1L):");
+        System.out.println("--------------------------------");
+        System.out.println(operario);
+        System.out.println();
+
+        // fetch Operarios by last name
+        List<Operario> bauers = repository.findByNombre("Bauer");
+        System.out.println("Operario found with findByLastName('Bauer'):");
+        System.out.println("--------------------------------------------");
+        for (Operario bauer : bauers) {
+            System.out.println(bauer);
+        }
+
+       // context.close();
+ 
     }
   /*  
    @Bean

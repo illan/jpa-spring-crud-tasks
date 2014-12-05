8281806664c7ff6da7de569995e579e8c9245e2c
illan palao <illanpalao@gmail.com>
2014-12-05 08:58:45 +0000

    spring-boot

--- spring-cli/src/main/java/org/tasks/Application.java
+++ spring-cli/src/main/java/org/tasks/Application.java
@@ -13,4 +13,17 @@ public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+/*    
+   @Bean
+   public JacksonRepositoryPopulatorFactoryBean repositoryPopulator() {
+
+    Resource sourceData = new ClassPathResource("test-data.json");
+
+    JacksonRepositoryPopulatorFactoryBean factory = new JacksonRepositoryPopulatorFactoryBean();
+    // Set a custom ObjectMapper if Jackson customization is needed
+    //factory.setObjectMapper(…);
+    factory.setResources(new Resource[] { sourceData });
+    return factory;
+  }
+ */ 
 }
